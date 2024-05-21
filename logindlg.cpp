3d0206b89629adddb20f13d6a1cbaf4e1825fad6
wz <wangzheszu@163.com>
2024-05-21 15:07:18 +0800

    2212

---
+++ logindlg.cpp
@@ -0,0 +1,14 @@
+#include "logindlg.h"
+#include "ui_logindlg.h"
+
+loginDlg::loginDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::loginDlg)
+{
+    ui->setupUi(this);
+}
+
+loginDlg::~loginDlg()
+{
+    delete ui;
+}
